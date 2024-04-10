b2c78791fb772e2ad25b37b70dfdf3a25a8a3a6b
GitIvanHubWhyNot <153542620+GitIvanHubWhyNot@users.noreply.github.com>
2024-04-10 14:53:34 +0300

    Add files via upload

---
+++ quiz_data.py
@@ -0,0 +1,32 @@
+quiz_data = [
+    {
+        "question": "What is the biggest city in Europe?",
+        "choices": ["London", "Paris", "Moscow", "Rome"],
+        "answer": "Moscow"
+    },
+    {
+        "question": "What is the name of the first president of the USSR?",
+        "choices": ["Gorbachev", "Brezhnev", "Lenin", "Stalin"],
+        "answer": "Gorbachev"
+    },
+    {
+        "question": "Who invented rocket fuel?",
+        "choices": ["Alferov", "Kit", "Korolev", "Chertovskiy"],
+        "answer": "Kit"
+    },
+    {
+        "question": "When was the first constitution declared?",
+        "choices": ["1791", "1787", "1789", "1710"],
+        "answer": "1787"
+    }
+]
+
+
+
+
+
+
+
+
+
+
